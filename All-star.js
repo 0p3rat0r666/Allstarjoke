e66cb22790b8608e088cdd5163b885a080a17675
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-12-16 20:26:59 -0600

    Update All-star.js

--- All-star.js
+++ All-star.js
@@ -2,11 +2,11 @@ var i = 0;
 const printMe = ["Somebody", "once", "told", "me",
 "The", "world", "is", "gonna", "roll", "me", "I", "ain't", "the", "sharpest", "tool", "in", "the", "shed", "she", "was", "looking", "kind", "of", "dumb", "With", "her", "finger", "and", "her", "thumb", "In", "the", "shape", "of", "an", '"L"', "on", "her", "forehead"];
 function play(){
-  If(i === printMe.length){
+  if(i === printMe.length){
      console.log(":3");
      clearInterval(playing);
   }else{
-     console.log(printMe[I]);
+     console.log(printMe[i]);
      i = i + 1;
   }
 }
